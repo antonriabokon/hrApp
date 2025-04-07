a42934ccb9bd156d6729e6e160f1d999176fc780
antonriabokon <s2500139@edu.bc.fi>
2025-04-07 11:42:59 +0300

    adding more cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import './App.css'
 
 const App = () => {
   const people = [
-    { name: 'Alice', occupation: 'Engineer', age: 28, pet: 'Dog' },
-    { name: 'Bob', occupation: 'Designer', age: 34, pet: 'Cat' },
-    { name: 'Charlie', occupation: 'Teacher', age: 41, pet: 'Parrot' },
-    { name: 'Diana', occupation: 'Doctor', age: 30, pet: 'Rabbit' },
-    { name: 'Lisa', occupation: 'Jornalist', age: 29, pet: 'Fox' }
+    { name: 'Alice', occupation: 'Engineer', salary: 2800, phone: '0401232323', email: 'alice@email.com', pet: 'Dog' },
+    { name: 'Bob', occupation: 'Designer', salary: 3400, phone: '0410899827', email: 'bob@email.com', pet: 'Cat' },
+    { name: 'Charlie', occupation: 'Teacher', salary: 4100, phone: '0403879655', email: 'charlie@email.com', pet: 'Fish' },
+    { name: 'Diana', occupation: 'Doctor', salary: 3000, phone: '0409887234', email: 'diana@email.com', pet: 'Eagle' },
+    { name: 'Lisa', occupation: 'Jornalist', salary: 2900, phone: '0414495676', email: 'lisa@email.com', pet: 'Fox' },
+    { name: 'John', occupation: 'Software developer', salary: 5900, phone: '0403557423', email: 'john@email.com', pet: 'Dinosaur' },
+    { name: 'Lara', occupation: 'Police officer', salary: 3600, phone: '0403771122', email: 'lara@email.com', pet: 'Horse' },
+    { name: 'Timo', occupation: 'Hairdresser', salary: 1900, phone: '0528763561', email: 'timo@email.com', pet: 'Snake' }
   ];
 
   return (
     <div>
       <header>
-        <h1>Personal Info Cards</h1>
+        <h1>HR application</h1>
       </header>
 
       <main>
@@ -24,14 +27,16 @@ const App = () => {
           <div className="card" key={index}>
             <h2>{person.name}</h2>
             <p>Occupation: {person.occupation}</p>
-            <p>Age: {person.age}</p>
+            <p>Salary: {person.salary}</p>
+            <p>Phone: {person.phone}</p>
+            <p>Email: {person.email}</p>
             <p>Pet: {person.pet}</p>
           </div>
         ))}
       </main>
 
       <footer>
-        <p>&copy; 2025</p>
+        <p>&copy; 2025. REACT25K. Business College Helsinki</p>
       </footer>
     </div>
   );
